825bf05be07026e0031aa7c92e45edcc75af0c94
moromoi <alex.palchisky@gmail.com>
2023-02-10 22:25:34 +0200

    change config file

--- src/main/java/com/targil/tiba/Setup.java
+++ src/main/java/com/targil/tiba/Setup.java
@@ -2,6 +2,15 @@ package com.targil.tiba;
 import org.aeonbits.owner.Config;
 public interface Setup extends Config {
 
-    @DefaultValue("Firefox") // Chrome | Firefox | Edge | Opera |
+    @DefaultValue("Chrome") // Choose you browser: Chrome | Firefox | Edge | Opera
     String browser();
+
+    @DefaultValue("")
+    String username();
+
+    @DefaultValue("")
+    String password();
+
+    @DefaultValue("")
+    String key();
 }
